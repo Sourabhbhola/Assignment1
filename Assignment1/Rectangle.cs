f4f10d03fa75e8ac22f7931198f44b435ceddc54
Sourabh bhola <Sbhola3805@conestogac.on.ca>
2019-01-31 19:02:59 -0500

    Full Running Software

---
+++ Assignment1/Rectangle.cs
@@ -0,0 +1,120 @@
+﻿
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace Assignment1
+
+{
+
+    public class Rectangle
+
+    {
+
+        private int length; 
+
+        private int width;
+
+
+
+        public int Length { get => length; set => length = value; }
+
+        public int Width { get => width; set => width = value; }
+
+
+
+        public Rectangle() // default constructor
+
+        {
+
+            length = 1;
+
+            width = 1;
+
+        }
+
+
+
+        public Rectangle(int length, int width) // non-default constructor
+
+        {
+
+            this.length = length;
+
+            this.width = width;
+
+        }
+
+        public int GetLength() // method
+
+        {
+
+            return length;
+
+        }
+
+
+
+        public int SetLength(int length)
+
+        {
+
+            this.length = length;
+
+            return length;
+
+        }
+
+
+
+        public int GetWidth()
+
+        {
+
+            return width;
+
+        }
+
+        public int SetWidth(int width)
+
+        {
+
+            this.width = width;
+
+            return width;
+
+        }
+
+        public int GetPerimeter()
+
+        {
+
+            return 2 * (length + width);
+
+
+
+
+
+        }
+
+        public int GetArea()
+
+        {
+
+            return length * width;
+
+        }
+
+
+
+    }
+
+}
